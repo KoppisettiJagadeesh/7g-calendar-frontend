1910f2198565ef40725b821fddd82953a26ad6e1
saleemjavid360 <msaleem@kodecorp.com>
2025-03-10 10:31:30 +0530

    CalendarActionsNavBar

--- src/pages/calendar/Calendar.Context.jsx
+++ src/pages/calendar/Calendar.Context.jsx
@@ -2,13 +2,15 @@ import { createContext, useState } from 'react';
 const CalendarContext = createContext();
 const CalendarProvider = ({ children }) => {
   const [calendarType, setCalendarType] = useState({ name: 'Week', code: 'Week' });
+  // const [calendarType, setCalendarType] = useState({ name: 'Month', code: 'Month' });
+  // const [calendarType, setCalendarType] = useState({ name: 'Day', code: 'Day' });
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [selectedDate, setSelectedDate] = useState(null);
 
-  const updateCalendarType = type => {
+  const updateCalendarType = (type) => {   
     setCalendarType(type);
   };
-  const updateSelectedDate = date => {
+  const updateSelectedDate = (date) => {
     setSelectedDate(date);
   };
   return <CalendarContext.Provider value={{ calendarType, updateCalendarType, updateSelectedDate, selectedDate }}>{children}</CalendarContext.Provider>;
